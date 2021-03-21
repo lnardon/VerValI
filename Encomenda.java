6150d4e84ac9f59e523f87b5ed1d0fab034fc95b
lnardon <lucaslnardon@gmail.com>
2021-03-21 20:33:47 -0300

    :tada:First commit

---
+++ Encomenda.java
@@ -0,0 +1,13 @@
+class Encomenda {
+    private int umKg;
+    private int cincoKg;
+
+    public Encomenda(int estUmKg, int estCincoKg){
+        umKg = estUmKg;
+        cincoKg = estCincoKg;
+    };
+
+    public int[] qtdDeBarras(int peso){
+
+    };
+}
