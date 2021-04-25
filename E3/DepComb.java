3c7886ae1f042ac02d7fd933b4685034e80391e0
lnardon <lucaslnardon@gmail.com>
2021-04-25 04:17:42 -0300

    ✨ Add base DepComb class for E3

---
+++ E3/DepComb.java
@@ -0,0 +1,53 @@
+public class DepComb {
+    public enum SITUACAO {NORMAL,SOBRAVISO,EMERGENCIA}
+    public enum TIPOPOSTO {COMUM, ESTRATEGICO}
+
+    public static final int MAX_ADITIVO = 500;
+    public static final int MAX_ALCOOL = 5200;
+    public static final int MAX_GASOLINA = 10000;
+
+    public DepComb(int tAditivo, int tGasolina, int tAlcool1, int tAlcool2) {
+
+    }
+
+    public void defineSituacao(){
+
+    }
+
+    public SITUACAO getSituacao(){
+
+    }
+
+    public int getGasolina(){
+        
+    }
+
+    public int getAditivo(){
+        
+    }
+
+    public int getAlcool1(){
+        
+    }
+
+    public int getAlcool2(){
+        
+    }
+
+    public int recebeAditivo(int qtdade){
+        
+    }
+
+    public int recebeGasolina(int qtdade){
+        
+    }
+
+    public int revcebeAlcool1(int qtdade){
+        
+    }
+
+    public int[] encomendaCombustivel(int qtdade, TIPOPOSTO tipoPosto){
+        
+    }
+
+}
